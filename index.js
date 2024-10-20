ac4b92e6a20355e8f1a1c1a716e89ca9a56a3dc1
vikneshwaran <vikneshwaran360@gmail.com>
2024-10-20 13:48:55 +0530

    main

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server,{
+  cors: {
+    origin:"*", 
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('Socket.io Chat Server');
+});
+io.on('connection', (socket) => {
+  console.log('New connection established');
+
+  socket.on('joinRoom', (room) => {
+    socket.join(room);
+    console.log(`User joined room: ${room}`);
+  });
+
+  socket.on('leaveRoom', (room) => {
+    socket.leave(room);
+    console.log(`User left room: ${room}`);
+  });
+
+  socket.on('chatMessage', ({ room, message, username }) => {
+    io.to(room).emit('message', { username, message });
+  });
+});
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
